d22ee4c2822d47f67068254dae94d9b2989fc499
Fatma-Nur-Evrensel <134015731+Fatma-Nur-Evrensel@users.noreply.github.com>
2023-05-19 01:20:34 +0300

    Multiplication
    In this process, multiplication has been added to the project.

--- Calculator.py
+++ Calculator.py
@@ -8,20 +8,25 @@ def toplama(sayi1, sayi2):
     return sayi1 + sayi2
 def cikarma(sayi1, sayi2):
     return sayi1 - sayi2
+def carpma(sayi1, sayi2):
+    return sayi1 * sayi2
 print("Hesap Makinesi Programına Hoş Geldiniz!")
 
 while True:
     print("İşlem Seçin:")
     print("1. Toplama")
     print("2. Çıkarma")
+    print("3. Çarpma")
 
-    secim = input("Seçiminizi yapın (1/2): ")
+    secim = input("Seçiminizi yapın (1/2/3): ")
     sayi1 = float(input("Birinci sayıyı girin: "))
     sayi2 = float(input("İkinci sayıyı girin: "))
     if secim == '1':
         print("Sonuç: ", toplama(sayi1, sayi2))
     elif secim == '2':
         print("Sonuç: ", cikarma(sayi1, sayi2))
+    elif secim == '3':
+        print("Sonuç: ", carpma(sayi1, sayi2))
     print("------------------------")
 
 
