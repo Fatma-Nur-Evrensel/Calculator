8a7efcb9e59a3473a703e66481595cf0b41bace5
Fatma-Nur-Evrensel <134015731+Fatma-Nur-Evrensel@users.noreply.github.com>
2023-05-19 01:26:09 +0300

    Version 1.0
    Version 1.0 completed.

--- Calculator.py
+++ Calculator.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 def toplama(sayi1, sayi2):
     return sayi1 + sayi2
+
 def cikarma(sayi1, sayi2):
     return sayi1 - sayi2
+
 def carpma(sayi1, sayi2):
     return sayi1 * sayi2
+
 def bolme(sayi1, sayi2):
     if sayi2 != 0:
         return sayi1 / sayi2
     else:
         return "Hata: Sıfıra bölme hatası!"
+
 print("Hesap Makinesi Programına Hoş Geldiniz!")
 
 while True:
@@ -23,10 +27,17 @@ while True:
     print("2. Çıkarma")
     print("3. Çarpma")
     print("4. Bölme")
+    print("5. Çıkış")
 
-    secim = input("Seçiminizi yapın (1/2/3/4): ")
+    secim = input("Seçiminizi yapın (1/2/3/4/5): ")
+
+    if secim == '5':
+        print("Hesap makinesi kapatılıyor...")
+        break
+
     sayi1 = float(input("Birinci sayıyı girin: "))
     sayi2 = float(input("İkinci sayıyı girin: "))
+
     if secim == '1':
         print("Sonuç: ", toplama(sayi1, sayi2))
     elif secim == '2':
@@ -35,6 +46,9 @@ while True:
         print("Sonuç: ", carpma(sayi1, sayi2))
     elif secim == '4':
         print("Sonuç: ", bolme(sayi1, sayi2))
+    else:
+        print("Geçersiz seçim! Lütfen tekrar deneyin.")
+
     print("------------------------")
 
 
