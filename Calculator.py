518d886b240440fbfc489a9255cc3d05a7bd3d88
Fatma-Nur-Evrensel <134015731+Fatma-Nur-Evrensel@users.noreply.github.com>
2023-05-19 01:23:16 +0300

    Divide
    In this process, the dividing operation is added to the project.

--- Calculator.py
+++ Calculator.py
@@ -10,6 +10,11 @@ def cikarma(sayi1, sayi2):
     return sayi1 - sayi2
 def carpma(sayi1, sayi2):
     return sayi1 * sayi2
+def bolme(sayi1, sayi2):
+    if sayi2 != 0:
+        return sayi1 / sayi2
+    else:
+        return "Hata: Sıfıra bölme hatası!"
 print("Hesap Makinesi Programına Hoş Geldiniz!")
 
 while True:
@@ -17,8 +22,9 @@ while True:
     print("1. Toplama")
     print("2. Çıkarma")
     print("3. Çarpma")
+    print("4. Bölme")
 
-    secim = input("Seçiminizi yapın (1/2/3): ")
+    secim = input("Seçiminizi yapın (1/2/3/4): ")
     sayi1 = float(input("Birinci sayıyı girin: "))
     sayi2 = float(input("İkinci sayıyı girin: "))
     if secim == '1':
@@ -27,6 +33,8 @@ while True:
         print("Sonuç: ", cikarma(sayi1, sayi2))
     elif secim == '3':
         print("Sonuç: ", carpma(sayi1, sayi2))
+    elif secim == '4':
+        print("Sonuç: ", bolme(sayi1, sayi2))
     print("------------------------")
 
 
